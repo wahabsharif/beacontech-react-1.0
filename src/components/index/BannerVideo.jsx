81d3d269d161b163912efd0603c1eb702513ef8b
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-03-23 00:23:26 +0500

    banner video

--- src/components/index/BannerVideo.jsx
+++ src/components/index/BannerVideo.jsx
@@ -4,27 +4,41 @@ import { FiPause, FiPlay } from "react-icons/fi";
 const BannerVideo = () => {
   const videoRef = useRef(null); // Ref to access the video element
   const [isPlaying, setIsPlaying] = useState(false);
+  const [showButton, setShowButton] = useState(true);
 
   const handlePlayPause = () => {
     if (videoRef.current.paused) {
       videoRef.current.play();
       setIsPlaying(true);
+      setShowButton(false); // Hide the button when video starts playing
     } else {
       videoRef.current.pause();
       setIsPlaying(false);
+      setShowButton(true); // Show the button when video is paused
     }
+  };
+
+  const handleVideoEnd = () => {
+    setIsPlaying(false); // Set isPlaying to false when video ends
+    setShowButton(true); // Show the button again when video ends
   };
 
   return (
     <>
       <div className="video-container">
-        <video ref={videoRef} className="video-fullscreen">
+        <video
+          ref={videoRef}
+          className="video-fullscreen"
+          onEnded={handleVideoEnd}
+        >
           <source src="video/waqar-jutt-bt-intro.webm" />
         </video>
         <div className="video-content">
-          <button onClick={handlePlayPause}>
-            {isPlaying ? <FiPause /> : <FiPlay />}
-          </button>
+          {showButton && ( // Render button conditionally based on showButton state
+            <button onClick={handlePlayPause}>
+              {isPlaying ? <FiPause /> : <FiPlay />}
+            </button>
+          )}
         </div>
       </div>
     </>
