a3d8980ad917377098ce78f775f45eb0fb4f30d6
wahab <www.rex19@gmail.com>
2024-06-27 14:17:57 +0500

    Business Revamp: Footer

---
+++ src/components/business-revamp/WhyChooseUs.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const WhyChooseUs = () => {
+  return (
+    <section>
+      <div className="br-container">
+        <div className="why-us">
+          <h2>Why Choose Us?</h2>
+          <p>
+            <strong>Expertise:</strong> Our team of seasoned professionals
+            brings years of experience in various industries. Customization: We
+            tailor our services to fit your unique business needs and goals.
+            Results-Driven: Focused on delivering measurable improvements and
+            tangible results. Innovative Solutions: We leverage the latest
+            trends and technologies to keep you ahead of the curve.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WhyChooseUs;
--- src/views/business-revamp.jsx
+++ src/views/business-revamp.jsx
@@ -1,10 +1,15 @@
 // business-revamp.jsx
-import React from "react";
+import React, { Suspense } from "react";
 import "../../src/assets/css/business-revamp.css";
 import BRNavBar from "../components/business-revamp/BRNavBar";
 import Banner from "../components/business-revamp/Banner";
 import OurModel from "../components/business-revamp/OurModel";
 import UniversalExamples from "../components/business-revamp/UniversalExamples";
+import WhyChooseUs from "../components/business-revamp/WhyChooseUs";
+import Address from "../components/Address";
+
+const Contact = React.lazy(() => import("../components/Contact"));
+const Footer = React.lazy(() => import("../components/footer/Footer"));
 
 const BusinessRevamp = () => {
   return (
@@ -15,6 +20,52 @@ const BusinessRevamp = () => {
       <Banner />
       <OurModel />
       <UniversalExamples />
+      <WhyChooseUs />
+      <div className="beny_tm_contact dark-bg" id="contact">
+        <div className="container">
+          <div className="contact_inner">
+            <div
+              className="left"
+              data-aos="fade-right"
+              data-aos-duration="1200"
+            >
+              <div className="beny_tm_title_holder">
+                <span>Contact</span>
+                <h3>Crafting Excellence in Your Project</h3>
+              </div>
+              <div className="short_list">
+                <Address />
+              </div>
+            </div>
+            <div
+              className="right"
+              data-aos="fade-right"
+              data-aos-duration="1200"
+              data-aos-delay="150"
+            >
+              <div className="title">
+                <p>
+                  We are always open
+                  <br />
+                  <span>to interaction.</span>
+                </p>
+              </div>
+              <div className="fields">
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Contact />
+                </Suspense>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="beny_tm_copyright">
+        <div className="container">
+          <Suspense fallback={<div>Loading...</div>}>
+            <Footer />
+          </Suspense>
+        </div>
+      </div>{" "}
     </div>
   );
 };
