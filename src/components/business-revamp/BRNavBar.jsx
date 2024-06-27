3387473f808ff87de0d854c749e09db8c55886cc
wahab <www.rex19@gmail.com>
2024-06-27 15:40:43 +0500

    Business Revamp: Navbar Responsive

--- src/components/business-revamp/BRNavBar.jsx
+++ src/components/business-revamp/BRNavBar.jsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const BRNavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="nav-bar-container">
       <div className="nav-bar">
-        <div className="logo">
+        <div className={`logo ${isMenuOpen ? "hide" : ""}`}>
           <img src="/img/logo/bt-logo-light.png" alt="Beacon Tech Logo" />
         </div>
-        <Link to="/" className="nav">
-          Home
-        </Link>
-        {/* <Link to="#" className="nav">
-          Search
-        </Link>
-        <Link to="#" className="nav">
-          Following
-        </Link> */}
-        <Link to="/contact" className="nav">
-          Contact
-        </Link>
+        <div className={`nav-links ${isMenuOpen ? "open" : ""}`}>
+          <Link to="/" className="nav">
+            Home
+          </Link>
+          <Link to="/" className="nav">
+            About
+          </Link>
+          <Link to="/" className="nav">
+            Portfolio
+          </Link>
+          <Link to="/" className="nav">
+            Services
+          </Link>
+          <div className="nav-btn">
+            <Link to="https://wa.me/923188565612">Contact Us</Link>
+          </div>
+        </div>
+        <div
+          className={`hamburger ${isMenuOpen ? "open" : ""}`}
+          onClick={toggleMenu}
+        >
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
       </div>
     </div>
   );
