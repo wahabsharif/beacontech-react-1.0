560818d677496b04965b03d0fce2a5f17ca9085b
wahab <www.rex19@gmail.com>
2024-06-27 10:14:25 +0500

    Business Revamp: Banner

---
+++ src/components/business-revamp/Banner.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <section className="section banner-section">
+      <div className="container banner-column">
+        <div className="banner-inner">
+          <h1 className="heading-xl">Business Revamp.</h1>
+          <p className="paragraph">
+            Transform your business with our comprehensive Business Revamp
+            Services designed to propel your company to new heights. Our expert
+            team will work closely with you to identify areas for improvement,
+            streamline operations, and enhance overall efficiency. Whether
+            you’re looking to innovate, enter new markets, or improve customer
+            satisfaction, we provide tailored solutions to meet your unique
+            needs.
+          </p>
+          <button className="btn btn-darken btn-inline">Get Started</button>
+        </div>
+        <img
+          className="banner-image"
+          src="https://i.ibb.co/sVqYmS2/Illustration.png"
+          alt="Illustration"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
--- src/views/business-revamp.jsx
+++ src/views/business-revamp.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import "../../src/assets/css/business-revamp.css";
 import BRNavBar from "../components/business-revamp/BRNavBar";
+import Banner from "../components/business-revamp/Banner";
 
 const BusinessRevamp = () => {
   return (
@@ -9,6 +10,7 @@ const BusinessRevamp = () => {
       <div className="background"></div>
       <div className="overlay"></div>
       <BRNavBar />
+      <Banner />
     </div>
   );
 };
